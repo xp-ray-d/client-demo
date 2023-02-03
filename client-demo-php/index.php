586a1850dbc5a06ad66e89033127f41ad1cc4d16
ray <dlmemr@163.com>
2023-02-03 15:40:36 +0800

    1、增加验签处理

--- client-demo-php/index.php
+++ client-demo-php/index.php
@@ -7,7 +7,7 @@
     //平台分配给客户的唯一识别号
     $cusCode = "C0000035C0001";
     //平台公钥
-    $platPublicKey = "";
+    $platPublicKey = file_get_contents("public.key");
     //客户私钥
     $cusPrivateKey = file_get_contents("C0000035C0001_PKCS1.key");
 
@@ -17,9 +17,11 @@
     $msgPublic["cusTraceNo"] = md5(uniqid(mt_rand(), true));
     $msgPublic["cusCode"]    = $cusCode;
 
+    file_put_contents(date("Y-m-d") . ".txt", $msgPublic["cusTraceNo"]);
+
     //私有参数体
     $msgPrivate["origTxnDate"]    = date("Ymd");//支付订单日期
-    $msgPrivate["origCusTraceNo"] = "fd8bb6031335888eed3670a073978ce1";//支付订单的客户订单号
+    $msgPrivate["origCusTraceNo"] = "afcc86f2bd7d475024e6d5c95e356402";//支付订单的客户订单号
     $msgPrivate["origSysTraceNo"] = "";
 
     $msgPrivate2["txnAmt"]             = "1";
@@ -52,15 +54,57 @@
     //报文体需要转成json再进行签名
     $sign = getRsaSign(json_encode($body, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE), $cusPrivateKey);
 
+    //组装接口请求数据
     $data = [
         "body" => $body,
         "sign" => $sign,
     ];
 
+    //请求接口
     $result = curl_request($url, $data);
+
+    //获取返回的签名
+    $return_sign = $result["sign"];
+
+    //校验签名，注意$result["body"]已是字符串，无需再json编码
+    $verifySign = verifySign($result["body"], $return_sign, $platPublicKey);
+    if (!$verifySign) {//验签失败
+        die("验签失败");
+    }
+
+    //对结果body层做json解码处理
     $result["body"] = json_decode($result["body"], true);
 
     die(json_encode($result));
+
+    /**
+     * 校验签名
+     * @param $content   [接口返回的body的键值字符串]
+     * @param $sign      [接口返回的签名]
+     * @param $publicKey [平台公钥]
+     * @return bool
+     * @author sensus
+     */
+    function verifySign($content, $sign, $publicKey)
+    {
+        $sign      = base64_decode($sign);
+        $publicKey = "-----BEGIN PUBLIC KEY-----\n" .
+            wordwrap($publicKey, 64, "\n", true) .
+            "\n-----END PUBLIC KEY-----";
+
+        $pubkeyid  = openssl_get_publickey($publicKey);
+
+        if ($pubkeyid) {
+            //返回值：如果签名正确则返回1，如果不正确则返回0，错误时返回-1或false。
+            $verify = openssl_verify($content, $sign, $pubkeyid, "sha256");
+            openssl_free_key($pubkeyid);
+            if ($verify == 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     /**
      * 获取签名
@@ -86,16 +130,16 @@
     /**
      * curl请求
      * @param string $url
-     * @param array $SendData
-     * @param int $json_type
+     * @param $sendData
+     * @param int $jsonType
      * @param array $headerArray
      * @return mixed|string
      * @author sensus
      */
-    function curl_request($url, $SendData, $json_type = 1, $headerArray = [])
+    function curl_request($url, $sendData, $jsonType = 1, $headerArray = [])
     {
-        if ($json_type) {
-            $SendData    = json_encode($SendData);
+        if ($jsonType) {
+            $sendData    = json_encode($sendData);
             $headerArray = ["Content-type:application/json;charset=UTF-8"];
         }
         $curl = curl_init();
@@ -103,7 +147,7 @@
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
         curl_setopt($curl, CURLOPT_POST, 1);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $SendData);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $sendData);
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headerArray);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);
@@ -111,10 +155,22 @@
         $output = curl_exec($curl);
         if ($output) {
             curl_close($curl);
+            $output = str_replace("\\\\\\", "\\", $output);//处理多斜杠bug
             return json_decode($output, true);
         } else {
             $error = 'Curl error: ' . curl_error($curl);
             curl_close($curl);
             return $error;
         }
+    }
+
+    /**
+     * 断点调试
+     * @param $data
+     * @author sensus
+     */
+    function dd($data)
+    {
+        var_dump($data);
+        die;
     }
